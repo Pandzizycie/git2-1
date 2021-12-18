4a77e241be872de6e55bcd6816f50279c1e9626c
Pandzizycie <pandzizycie@gmail.com>
2021-12-18 14:22:50 +0000

    v3.0 Kamila

--- testy.py
+++ testy.py
@@ -1,19 +1,44 @@
-sex = input('[k]obieta czy [m]ezczyzna ?:')
-if sex == 'k':
-    print('mozesz uzywac aplikacje Droga Pani')
-    wiek = input("Podaj wiek: ")
-    # Sprawdzamy, czy wiek jest złożony z cyfr
-    if wiek.isdigit() == False:
-        exit("Wiek musi być liczbą")
-    wiek = int(wiek)
-    if wiek >= 21 and wiek <= 65:
-        print("Witamy w aplikacji i smacznej degustacji!")
-    elif wiek > 65:
-        print("Jesteś już za stary/a na alkohol")
-    else:
-        exit('jesteś za młody/a!, w USA wymagamy 21 lat')
-elif sex =='m':
-    print('Ta aplikacja nie jest dla facetow, sorry!')
+lok = input("Podaj lokalizacje [e] (Europa) lub [us] (Stany Zjednoczone): ")
+if lok == 'e':
+    plec = input("Podaj plec [K] (kobieta) lub [M] (mezyczyzna): ")
 
+    if plec == 'K':
+        wiek = input("Podaj wiek: ")
+        # Sprawdzamy, czy wiek jest złożony z cyfr
+        if wiek.isdigit() == False:
+            exit("Wiek musi być liczbą")
+        wiek = int(wiek)
+        if wiek >= 18 and wiek <= 65:
+            print("Witamy!")
+        elif wiek > 65:
+            print("Jesteś już za stara na alkohol")
+        else:
+            exit('jesteś za młoda! W USA wymagamy 21 lat')
+
+    elif plec == "M":
+        print("Ta alikacja nie jest dla facetow, sorry")
+    else:
+        exit("Wybrano nieobslugiwana wartosc. Aplikacja jest tylko dla pelnoletnich kobiet. Sprobuj ponownie")
+
+elif lok == 'us':
+    plec = input("Podaj plec [K] (kobieta) lub [M] (mezyczyzna): ")
+
+    if plec == 'K':
+        wiek = input("Podaj wiek: ")
+        # Sprawdzamy, czy wiek jest złożony z cyfr
+        if wiek.isdigit() == False:
+            exit("Wiek musi być liczbą")
+        wiek = int(wiek)
+        if wiek >= 21 and wiek <= 65:
+            print("Witamy!")
+        elif wiek > 65:
+            print("Jesteś już za stara na alkohol")
+        else:
+            exit('jesteś za młoda! W USA wymagamy 21 lat')
+
+    elif plec == "M":
+        print("Ta alikacja nie jest dla facetow, sorry")
+    else:
+        exit("Wybrano nieobslugiwana wartosc. Aplikacja jest tylko dla pelnoletnich kobiet. Sprobuj ponownie")
 else:
-    print('Wybrano nieoslugiwana wartosci. Aplikacja jest tylko dla pelnoletnich kobiet. Sprobuj ponownie')
+    exit("Wybrano nieobslugiwana wartosc. Aplikacja jest tylko dla mieszkancow Europy lub Stanow Zjednoczonych. Sprobuj ponownie")
