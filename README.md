1bde0fa5b432ce314fb65e940a76db9bab37507a
testerpodyplomowe <testerpodyplomowe@gmail.com>
2021-12-18 12:30:25 +0100

    update to 2.1

--- README.md
+++ README.md
@@ -12,4 +12,10 @@ v.1.0
 
 v.1.1
 - [x] zwiekszono limit wieku do 65
-- [x] wiek dla doroslych USA
+- [x] wiek dla doroslych w USA
+
+v.2.0
+- [ ] aplikacja obsluguje tylko kobiety pelnoletnie:)
+
+v.2.1
+- [ ] poprawiona walidacja dotyczaca wyboru plci
--- testy.py
+++ testy.py
@@ -1,11 +1,19 @@
-wiek = input("Podaj wiek: ")
-# Sprawdzamy, czy wiek jest złożony z cyfr
-if wiek.isdigit() == False:
-    exit("Wiek musi być liczbą")
-wiek = int(wiek)
-if wiek >= 21 and wiek <= 65:
-    print("Witamy!")
-elif wiek > 65:
-    print("Jesteś już za stary na alkohol")
+sex = input('[k]obieta czy [m]ezczyzna ?:')
+if sex == 'k':
+    print('mozesz uzywac aplikacje Droga Pani')
+    wiek = input("Podaj wiek: ")
+    # Sprawdzamy, czy wiek jest złożony z cyfr
+    if wiek.isdigit() == False:
+        exit("Wiek musi być liczbą")
+    wiek = int(wiek)
+    if wiek >= 21 and wiek <= 65:
+        print("Witamy w aplikacji i smacznej degustacji!")
+    elif wiek > 65:
+        print("Jesteś już za stary/a na alkohol")
+    else:
+        exit('jesteś za młody/a!, w USA wymagamy 21 lat')
+elif sex =='m':
+    print('Ta aplikacja nie jest dla facetow, sorry!')
+
 else:
-    exit('jesteś za młody!, w USA wymagamy 21 lat')
+    print('Wybrano nieoslugiwana wartosci. Aplikacja jest tylko dla pelnoletnich kobiet. Sprobuj ponownie')
